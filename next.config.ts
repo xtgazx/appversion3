33e6eac49a94561c08d40e793727a943bcb4bce7
xtgazx <xtgazx@gmail.com>
2026-03-19 16:31:18 -0400

    Remove experimental appDir configuration

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  experimental: {
-    appDir: true,
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;

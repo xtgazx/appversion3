99a7d3583421d7fcadad39d83d20971dd45923c6
xtgazx <xtgazx@gmail.com>
2026-03-21 14:56:21 -0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
+import { ClerkProvider, SignInButton } from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +28,14 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col"><ClerkProvider>{children}</ClerkProvider></body>
+      <body className="min-h-full flex flex-col">
+
+<ClerkProvider>
+  <SignInButton />
+  {children}
+</ClerkProvider>
+
+</body>
     </html>
   );
 }
